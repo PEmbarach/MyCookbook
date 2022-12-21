ae9a8ab72453e85da9aab7aa389911b95e6c231d
Pablo Embarach <pabloembarach@gmail.com>
2022-12-21 15:22:10 +0000

    Listing recipes by name, id and date in database. Add search field, filter and pagination.

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Recipe
 
-admin.site.register(Recipe)
+
+class RecipeList(admin.ModelAdmin):
+    list_display = ('id', 'recipe_name', 'category', 'recipe_date')
+    list_display_links = ('id', 'recipe_name')
+    search_fields = ('recipe_name',)
+    list_filter = ('category',)
+    list_per_page = 2
+
+
+admin.site.register(Recipe, RecipeList)
