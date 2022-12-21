01c126481ec01ac87dea33e9e2e76807efba12a0
Pablo Embarach <pabloembarach@gmail.com>
2022-12-21 14:51:36 +0000

    Registering routes, adding daata to the database, displaying data from data base

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from datetime import datetime
 
-# Create your models here.
+
+class Recipe(models.Model):
+    recipe_name = models.CharField(max_length=200)
+    ingredients = models.TextField()
+    method_of_preparation = models.TextField()
+    time_of_preparation = models.IntegerField()
+    serves = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    recipe_date = models.DateField(default=datetime.now, blank=True)
--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_list_or_404, get_object_or_404
+from .models import Recipe
 
 
 def index(request):
-    return render(request, 'index.html')
+    recipes = Recipe.objects.all()
+
+    data = {
+        'recipes': recipes
+    }
+    return render(request, 'index.html', data)
 
 
-def recipe(request):
-    return render(request, 'recipe.html')
+def recipe(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+
+    show_recipe = {
+        'recipe': recipe
+    }
+
+    return render(request, 'recipe.html', show_recipe)
