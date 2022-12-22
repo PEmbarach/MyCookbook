d4277fc571825832a20432ddaa0fa4375b4a5db0
Pablo Embarach <pabloembarach@gmail.com>
2022-12-22 21:22:49 +0000

    Recipe form creation and linking with user

--- final_user/urls.py
+++ final_user/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('login', views.login, name='login'),
     path('dashboard', views.dashboard, name='dashboard'),
-    path('logout', views.logout, name='logout')
+    path('logout', views.logout, name='logout'),
+    path('create/recipe', views.create_recipe, name='create_recipe')
 ]
--- final_user/views.py
+++ final_user/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
+from django.contrib import auth
+from recipe.models import Recipe
+
 
 def signup(request):
     if request.method == 'POST':
@@ -28,12 +31,55 @@ def signup(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        if email == '' or password == '':
+            print('Os estao em branco')
+            return redirect('login')
+        print(email, password)
+        if User.objects.filter(email=email).exists():
+            name = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=name, password=password)
+            if user is not None:
+                auth.login(request, user)
+                print('Login realizado com sucesso')
+                return redirect('dashboard')
+
     return render(request, 'final_user/login.html')
 
 
-def logout():
-    pass
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
 
 
-def dashboard():
-    pass
+def dashboard(request):
+    if request.user.is_authenticated:
+        id = request.user.id
+        recipes = Recipe.objects.order_by('-recipe_date').filter(user=id)
+
+        data = {
+            'recipes': recipes
+        }
+
+        return render(request, 'final_user/dashboard.html', data)
+    else:
+        return redirect('index')
+
+
+def create_recipe(request):
+    if request.method == 'POST':
+        recipe_name = request.POST['recipe_name']
+        ingredients = request.POST['ingredients']
+        method_preparation = request.POST['method_preparation']
+        time_of_preparation = request.POST['time_of_preparation']
+        server = request.POST['server']
+        category = request.POST['category']
+        recipe_image = request.FILES['recipe_image']
+        user = get_object_or_404(User, pk=request.user.id)
+        recipe = Recipe.objects.create(user=user, recipe_name=recipe_name, ingredients=ingredients, method_of_preparation=method_preparation, time_of_preparation=time_of_preparation, serves=server, category=category, recipe_image=recipe_image)
+        recipe.save
+        return redirect('dashboard')
+    else:
+        return render(request, 'final_user/create_recipe.html')
